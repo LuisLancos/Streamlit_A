9a0487245ed53b3a5bd26069e7306c43a67c4a30
Lancos <luis.lancos@gmail.com>
2023-12-02 20:53:57 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,64 @@
 import streamlit as st
 import openai
 
-# Initialize OpenAI client
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]  # Use Streamlit Secrets to store the API key
-client = openai.Client(api_key=OPENAI_API_KEY)
+# Function to initialize OpenAI client with API key from Streamlit secrets
+def init_openai_client():
+    openai_api_key = st.secrets["OPENAI_API_KEY"]
+    return openai.Client(api_key=openai_api_key)
 
-# Define necessary functions (upload_file, create_thread, add_message_to_thread, etc.)
+# Function to create a new thread
+def create_thread(client):
+    thread = client.beta.threads.create()
+    return thread.id
 
+# Function to add a message to a thread
+def add_message_to_thread(client, thread_id, message_content):
+    client.beta.threads.messages.create(thread_id=thread_id, role="user", content=message_content)
+
+# Function to get responses from a thread
+def get_thread_responses(client, thread_id):
+    messages = client.beta.threads.messages.list(thread_id=thread_id, order="asc")
+    return [msg.content[0].text.value for msg in messages.data if msg.role == 'assistant']
+
+# Function to run the assistant and get a response
+def run_assistant_and_get_response(client, assistant_id, thread_id):
+    run = client.beta.threads.runs.create(assistant_id=assistant_id, thread_id=thread_id)
+    while run.status not in ['completed', 'failed']:
+        run = client.beta.threads.runs.retrieve(run_id=run.id, thread_id=thread_id)
+    if run.status == 'completed':
+        return get_thread_responses(client, thread_id)
+    else:
+        return ["No response or run failed"]
+
+# Streamlit app main function
 def main():
-    # Initialize session state variables if they don't exist
+    st.title("OpenAI Chatbot")
+
+    # Initialize OpenAI client
+    client = init_openai_client()
+
+    # Session state for thread management
     if 'thread_id' not in st.session_state:
-        st.session_state['thread_id'] = create_thread()
-    if 'file_ids' not in st.session_state:
-        st.session_state['file_ids'] = []
-
+        st.session_state['thread_id'] = create_thread(client)
+    
     # Text input for user query
-    user_input = st.text_input("Enter your query:")
+    user_query = st.text_input("Enter your query:")
 
-    # File uploader
-    uploaded_file = st.file_uploader("Upload a file", type=['pdf', 'txt', 'docx'])
-
-    # Handling file upload
-    if uploaded_file is not None:
-        file_id = upload_file(uploaded_file)
-        if file_id:
-            st.session_state['file_ids'].append(file_id)
-            st.write("File uploaded successfully.")
-
-    # Buttons for sending query, starting a new thread, etc.
+    # Handling the 'Send Query' button
     if st.button('Send Query'):
-        if user_input:
-            # Add message to thread and get response
-            # Display response
-            pass  # Implement this part
+        if user_query:
+            assistant_id = "asst_axrsu71yTNAXbzyf1Nv1EJ59"  # Replace with your actual assistant ID
+            add_message_to_thread(client, st.session_state['thread_id'], user_query)
+            response = run_assistant_and_get_response(client, assistant_id, st.session_state['thread_id'])
+            if response:
+                st.write("Assistant's Response:", response[0])
+            else:
+                st.write("Assistant's Response: No response received.")
 
+    # Handling the 'Start New Thread' button
     if st.button('Start New Thread'):
-        st.session_state['thread_id'] = create_thread()
-        st.session_state['file_ids'] = []
+        st.session_state['thread_id'] = create_thread(client)
 
-    if st.button('Exit'):
-        st.stop()
-
-# Running the main function
+# Run the Streamlit app
 if __name__ == "__main__":
     main()
